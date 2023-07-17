bf3a6a88866121db0afcf5063b6f75a7404d96cc
Pedro Alburqueque <pralburco@gmail.com>
2023-07-17 16:37:04 -0400

    linked mysql db and created Menu and Booking models

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -76,8 +76,15 @@ WSGI_APPLICATION = 'LittleLemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'littlelemon',
+        'USER': 'root1',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        }
     }
 }
 
